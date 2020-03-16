2160e294bee4eb61483a64611631b3206266a313
SyllaAbu <46777046+SyllaAbu@users.noreply.github.com>
2020-03-16 21:03:08 +0100

    Add files via upload

---
+++ Scuola/database.py
@@ -0,0 +1,20 @@
+books = [
+    {
+        'id': 0,
+        'title': 'Il nome della Rosa',
+        'author': 'Umerto Eco',
+        'year_published': '1980',
+    },
+    {
+        'id': 1,
+        'title': 'Il problema dei tre corpi',
+        'author': 'Liu Cixin',
+        'year_published': '2008',
+    },
+    {
+        'id': 2,
+        'title': 'Fondazione',
+        'author': 'Isaac Asimov',
+        'year_published': '1951',
+    },
+]
